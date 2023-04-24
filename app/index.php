cda3e9d67b5af70c449a4bc32448b94a70db84fe
Nguyen Hua <nguyenhua.hcm@gmail.com>
2023-04-24 23:36:15 +0700

    Update: Getting data

--- app/index.php
+++ app/index.php
@@ -1,5 +1,26 @@
 <?php 
-$rand = "This is a random string"
+$url = '';
+$message = '';
+$is_crawling = FALSE;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+  if (isset($_POST["start_crawl"])){
+    $url = $_POST["url"];
+    if($url != ''){
+      $is_crawling = TRUE;
+      $message = "Crawling at ". $url;
+      // Call function crawl here 
+    }
+  }elseif (isset($_POST["stop_crawl"])){
+    if($is_crawling) {
+      $message = "Stopped crawling";
+      $is_crawling = FALSE;
+      // Call function to stop crawl here
+    }
+  }elseif (isset($_POST["download"])){
+    // Call function to start download here
+  }
+}
 ?>
 
 <!DOCTYPE html>
@@ -14,28 +35,52 @@ $rand = "This is a random string"
 <body>
   <div class="container">
     <h1>Simple Web Crawler</h1>
-    <div class="container_action">
-      <input type="text" placeholder="Enter a url you want to crawl" />
-      <select>
-        <option>Choose file format</option>
-        <option disabled>---Image Extensions---</option>
-        <option>jpg</option>
-        <option>png</option>
-        <option>jpeg</option>
-        <option disabled>---File Extensions---</option>
-        <option>pdf</option>
-        <option>mp3</option>
-      </select>
-    </div>
+    <form action="index.php" method="POST" class="container_wrapper" id="input_form">
+      <div class="container_input">
+        <input name="url" id="url" type="text" placeholder="Enter a url you want to crawl" value=""/>
+        <select>
+          <option>Choose file format</option>
+          <option disabled>---Image Extensions---</option>
+          <option>jpg</option>
+          <option>png</option>
+          <option>jpeg</option>
+          <option disabled>---File Extensions---</option>
+          <option>pdf</option>
+          <option>mp3</option>
+        </select>
+      </div>
+      <div class="container_operation">
+        <input type="submit" name="start_crawl" value="Start to crawl"/>
+        <input type="submit" name="stop_crawl" value="Stop crawling"/>
+      </div>
+    </form>
     <div class="container_result">
+      <h2><?= $message?></h2>
       <h2>Result: </h2>
       <div class="result_box">
 
       </div>
-      <div class="result_buttons">
-        <button>Download as zip</button>
-      </div>
+      <form action="index.php" class="result_buttons" method="POST">
+        <input type="submit" name="download" value="Download as zip" />
+      </form>
     </div>
   </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script type="text/javascript" >
+    $(document).ready(function() {
+      $("form").on("submit", function(event) {
+        // $.ajax({
+        //   type: "POST",
+        //   url: "index.php",
+        //   data: $(this).serialize,
+        //   succes: function {
+        //     console.log("YAY")
+        //   }
+        // })
+        event.preventDefault()
+      })
+    })
+
+  </script>
 </body>
 </html>
