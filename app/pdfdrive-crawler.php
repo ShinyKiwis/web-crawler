564f9d7b3bb395d5f13d1edeac98a12a77bbe501
tonible14012002 <namanhble14012002@gmail.com>
2023-05-03 13:23:02 +0700

    add pdf download btn

--- app/pdfdrive-crawler.php
+++ app/pdfdrive-crawler.php
@@ -1,99 +1,104 @@
+<ul>
+
 <?php
+
+set_time_limit(0);
 
 require 'vendor/autoload.php';
 
-function get_data() {
-    $BASE_URL = "https://www.pdfdrive.com";
-    $http_client = new \GuzzleHttp\Client();
+// header('Content-Type: application/json; charset=utf-8');
+// header("Connection: keep-alive");
 
-    $url_queue = [];
-    array_push($url_queue, $BASE_URL);
+$BASE_URL = "https://www.pdfdrive.com";
+$http_client = new \GuzzleHttp\Client();
 
-    $visited_url = [];
-    $download_urls = [];
+$url_queue = [];
+array_push($url_queue, $BASE_URL);
 
-    libxml_use_internal_errors(true);
-    $digCount = 200;
-    $count = 1;
+$visited_url = [];
 
-    while (!empty($url_queue)) {
-        if ($count > $digCount) {
-            break;
-        }
-        echo  "<div>count: ".$count ."</div>";
+libxml_use_internal_errors(true);
+$digCount = 15;
+$count = 1;
 
-        $count += 1;
-        $current_url = array_shift($url_queue);
+$download_urls = [];
+while (!empty($url_queue)) {
+    if ($count > $digCount) {
+        break;
+    }
+    $count += 1;
+    $current_url = array_shift($url_queue);
 
-        echo  "<div>Current Url: ".$current_url ."</div>";
+    $visited_url[] = $current_url;
 
-        $visited_url[] = $current_url;
+    // if the url is a book url
+    if (preg_match('/-e\d+\.html$/', $current_url)) {
+        $download_site_url = preg_replace('/-e(\d+)\.html$/', '-d$1.html', $current_url);
+        $visited_url[] = $download_site_url;
+        array_unshift($url_queue, $download_site_url);
+    }
+    // handle url
+    try {
+        $response = $http_client->get($current_url);
+    }
+    catch (Exception $e) {
+        continue;
+    }
 
-        // if the url is a book url
-        if (preg_match('/-e\d+\.html$/', $current_url)) {
-            $download_site_url = preg_replace('/-e(\d+)\.html$/', '-d$1.html', $current_url);
-            $visited_url[] = $download_site_url;
-            array_unshift($url_queue, $download_site_url);
-        }
+    $html_string= (string)$response->getBody();
 
-        // handle url
-        try {
-            $response = $http_client->get($current_url);
-        }
-        catch (Exception $e) {
-            continue;
-        }
+    $doc = new DOMDocument();
+    $doc->loadHTML($html_string);
+    $xpath = new DOMXPath($doc);
 
-        $html_string= (string)$response->getBody();
+    if (preg_match('/-d\d+\.html$/', $current_url)) {
+        $scripts = $xpath->query("//script[string-length(normalize-space(text())) > 0 and not(@src)]");
 
-        $doc = new DOMDocument();
-        $doc->loadHTML($html_string);
-        $xpath = new DOMXPath($doc);
+        foreach ($scripts as $script) {
+            $script_content = $script->nodeValue;
+            if ($script_content !== null) {
+                preg_match('/\\$.get\("\\/ebook\\/broken",\\{id:\'(?P<id>[^\\\']*)\',session:\'(?P<session>[^\\\']*)\',r:\'(?P<r>[^\\\']*)\'\\}\\)/', $script_content, $matches);
+                if (!empty($matches)){
+                    $check_broken_url = $BASE_URL."/ebook/broken?id=".$matches["id"]."&session=".$matches["session"]."&r=".$matches["r"];
+                    try {
+                        $response = $http_client->get($check_broken_url);
+                    }
+                    catch (Exception $e) {continue;}
+                    $Resphtml_string = (string)$response->getBody();
+                    $respDoc = new DOMDocument();
+                    $respDoc->loadHTML($Resphtml_string);
+                    $respXpath = new DOMXPath($respDoc);
+                    $download_btn = $respXpath->query("//a[@type='button'][1]");
 
-        if (preg_match('/-d\d+\.html$/', $current_url)) {
-            $scripts = $xpath->query("//script[string-length(normalize-space(text())) > 0 and not(@src)]");
-            foreach ($scripts as $script) {
-                $script_content = $script->nodeValue;
-                if ($script_content !== null) {
-                    preg_match('/\\$.get\("\\/ebook\\/broken",\\{id:\'(?P<id>[^\\\']*)\',session:\'(?P<session>[^\\\']*)\',r:\'(?P<r>[^\\\']*)\'\\}\\)/', $script_content, $matches);
-                    if (!empty($matches)){
-                        $check_broken_url = $BASE_URL."/ebook/broken?id=".$matches["id"]."&session=".$matches["session"]."&r=".$matches["r"];
-                        try {
-                            $response = $http_client->get($check_broken_url);
-                        }
-                        catch (Exception $e) {continue;}
-                        $Resphtml_string = (string)$response->getBody();
-                        $respDoc = new DOMDocument();
-                        $respDoc->loadHTML($Resphtml_string);
-                        $respXpath = new DOMXPath($respDoc);
-                        $download_btn = $respXpath->query("//a[count(*)=1 and self::i and @type='button' and @href]")->item(0);
-                        // $download_urls[] = $download_btn->getAttribute("href");
-
-                        echo "Download ".$download_btn;
+                    if ($download_btn->length > 0) {
+                        $download_url =  $download_btn->item(0)->getAttribute("href");
+                        echo '<li style="flex">
+                            <h3>'.$current_url.'</h3>
+                            <div> Download: <a class="pdf_download" download="'.$current_url.'" href="'.$download_url.'"target="blank" >'.$download_url.'</a>
+                            </div>
+                            </li>';
                     }
                 }
             }
         }
+    }
 
-        $links = $xpath->query("//a[@href]");
+    $links = $xpath->query("//a[@href]");
 
-        var_dump($links);
-
-        foreach($links as $link) {
-            $url = rtrim($link->getAttribute("href"), "/");
-            if (str_starts_with($url, "/")) {
-                $url = $BASE_URL . $url;
-            }
-            if (!in_array($url, $visited_url)
-                && !in_array($url, iterator_to_array($url_queue)))
-                // && preg_match('/-d\d+\.html$/', $current_url))
-            {
-                array_push($url_queue, $url);
-            }
+    foreach($links as $link) {
+        $url = rtrim($link->getAttribute("href"), "/");
+        if (str_starts_with($url, "/")) {
+            $url = $BASE_URL . $url;
+        }
+        if (!in_array($url, $visited_url)
+            && !in_array($url, iterator_to_array($url_queue)))
+            // && preg_match('/-d\d+\.html$/', $current_url))
+        {
+            array_push($url_queue, $url);
         }
     }
+
 }
 
-get_data()
-
 ?>
+</ul>
